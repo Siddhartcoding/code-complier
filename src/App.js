085d3749dd4c7ada978d5d9e3a8a6f7bc1edc509
Siddharth jangir <137277893+Siddhartcoding@users.noreply.github.com>
2024-02-28 13:29:52 +0530

    creating file structure

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./index.css";
+function App() {
+  return (
+    <div className="w-screen h-screen flex items-center justify-center">
+      App
+    </div>
+  );
+}
+
+export default App;
